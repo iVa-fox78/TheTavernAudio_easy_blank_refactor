fa4b57a9eb8c81f1a09e68da83dc4f424dbcfad1
Ivanna Siechkina <isiechkina@edu.cdv.pl>
2026-01-10 13:56:53 +0100

    VCA

--- Assets/Scripts/VCA.cs
+++ Assets/Scripts/VCA.cs
@@ -12,6 +12,7 @@ public class VCA : MonoBehaviour
     private FMOD.Studio.VCA musicVCA;
     private FMOD.Studio.VCA tavernVCA;
     private FMOD.Studio.VCA outsideVCA;
+    private FMOD.Studio.VCA ambientVCA;
 
     // Flagi stanu wyciszenia.
     [SerializeField]
@@ -26,11 +27,11 @@ public class VCA : MonoBehaviour
     void Start()
     {
         // Pobiera VCAs z FMOD.
-        globalVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Mute");
+        globalVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Master");
         musicVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Music");
-        tavernVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Tavern_amb");
-        outsideVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Outside_amb");
-
+        tavernVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Tavern");
+        outsideVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Outside");
+        ambientVCA = FMODUnity.RuntimeManager.GetVCA("vca:/Ambient");
         // Ustawia początkową głośność.
         globalVCA.setVolume(DecibelToLinear(-100));
     }
@@ -53,6 +54,10 @@ public class VCA : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.P))
         {
             ToggleMute(outsideVCA, ref outsideMuteActive);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ToggleMute(ambientVCA, ref outsideMuteActive);
         }
     }
 
---
+++ Assets/Scripts/VCAControl.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VCAControl : MonoBehaviour
+{
+    private FMOD.Studio.VCA vca;
+    private Slider slider;
+
+    [SerializeField] private float vcaVolume;
+    void Start()
+    {
+        vca = FMODUnity.RuntimeManager.GetVCA("vca/Music");
+        vca.getVolume(out vcaVolume);
+    }
+    public void SetVolume(float volume) 
+    {
+        vca.setVolume(volume); 
+    }
+}
