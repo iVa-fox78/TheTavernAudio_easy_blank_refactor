1b794ed25ba9b9a9f49f58189ccfc620c2955fea
Ivanna Siechkina <isiechkina@edu.cdv.pl>
2026-01-13 19:18:03 +0100

    Update 2.0 (Smth)
    Sliders update

--- Assets/Scripts/VCAControl.cs
+++ Assets/Scripts/VCAControl.cs
@@ -6,14 +6,25 @@ public class VCAControl : MonoBehaviour
     private FMOD.Studio.VCA vca;
     private Slider slider;
 
+    [Header("Settings FMOD")]
+    [SerializeField] private string vcaPath;
+    [SerializeField] private string saveKey; //MusicVolume save. Save from the slider. Not that necessary thing but still
+
+    [Header("Settings Volume")]
     [SerializeField] private float vcaVolume;
     void Start()
     {
-        vca = FMODUnity.RuntimeManager.GetVCA("vca:/Music");
+        slider = GetComponent<Slider>();
+        vca = FMODUnity.RuntimeManager.GetVCA(vcaPath);
+
+        float saveVolume = PlayerPrefs.GetFloat(saveKey, 1); //Saving music Volume choose from Slider
         vca.getVolume(out vcaVolume);
+        slider.value = saveVolume;
     }
-    public void SetVolume(float volume) 
+    public void SetVolume(float volume)
     {
-        vca.setVolume(volume); 
+        vca.setVolume(volume);
+
+        PlayerPrefs.SetFloat(saveKey, volume);
     }
 }
